54679fe7fc4e9554fcad295ea46c77fcc46a7098
Carlos Ricardo Villena Cabrejos <cvillenacabrejos@gmail.com>
2021-12-27 21:21:12 -0500

    Agregaando el avion

---
+++ TU-VIAJE/js/alertas.js
@@ -0,0 +1,25 @@
+function alerta(){
+    Swal.fire({
+        title: '¿Desea selecionar este asiento?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Aceptar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Felicidades!',
+            'Tu asiento ha sido selecionado correctamente',
+            'success'
+          )
+        }
+      })
+}
+
+function ocupado(){
+    Swal.fire({
+        icon: 'error',
+        title: 'Lo sentimos este asiento está ocupado'
+      })
+}
